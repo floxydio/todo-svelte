d479828ee23022b05ffc6d39132a8c276dcabe51
floxydio <diooktar@gmail.com>
2022-07-21 00:54:59 +0700

    todo with svelte

---
+++ src/pages/Comments.svelte
@@ -0,0 +1,20 @@
+<script>
+    let dataComments = [];
+
+    fetch("https://jsonplaceholder.typicode.com/comments", {
+        method: "GET"
+    }).then(response => response.json()).then(data => {
+        dataComments = data;
+    });
+    
+</script>
+
+{#each dataComments.slice(0,10) as comment}
+    <div class="comment">
+        <h3>{comment.name}</h3>
+        <p>{comment.body}</p>
+    </div>
+{/each}
+
+
+<style></style>
